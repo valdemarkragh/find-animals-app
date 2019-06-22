1e2d316699e4884ea55a0bae30a4809c9d46ba95
Valdemar Kragh <valdemar.kragh@hotmail.com>
2019-06-22 22:58:39 +0200

    Update savelocation.php

--- savelocation.php
+++ savelocation.php
@@ -12,19 +12,14 @@
 require_once('db_con.php');	
 
 	$postdata = file_get_contents("php://input");
-    $request = json_decode($postdata);
+    	$request = json_decode($postdata);
 	@$description = $request->description;
-    @$lat = $request->lat;
-    @$long = $request->long;
+    	@$lat = $request->lat;
+    	@$long = $request->long;
     
-
-// $lat = 5.0;
-// $long = 6.0;
-
-	
-			$sql = 'INSERT INTO rehome (description, latitude, longitude) VALUES (?, ?, ?)';
-			$stmt = $con->prepare($sql);
-			$stmt->bind_param('sdd', $description, $lat, $long);
-			$stmt->execute();
+		$sql = 'INSERT INTO rehome (description, latitude, longitude) VALUES (?, ?, ?)';
+		$stmt = $con->prepare($sql);
+		$stmt->bind_param('sdd', $description, $lat, $long);
+		$stmt->execute();
 	
 ?>
